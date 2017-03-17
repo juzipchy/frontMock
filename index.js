e7467e4ad3eeffd598d79f2ac57d58a05d39450b
john <xiongmeiqiang@xiaomi.com>
2017-03-17 18:24:47 +0800

    add routes

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Mock = require('mockjs'),
 	_ = require('underscore'),
-	// delay = require('express-delay'),
+	delay = require('express-delay'),
 	posts = require('./route/post'),
 	http = require('http'),
 	gets = require('./route/get'),
 	express = require('express'),
 	fs = require('fs'),
+	actionRoutes = require('./route/actionroutes')
 	app = express();
-	// app.use(delay(100000))
+	app.use(delay(200, 1000))
+
 	_.each(posts, function (value, name) {
 	    app.post(name , function(req, res) {
 	    	let dataFormatted = JSON.parse(fs.readFileSync(`./data/${value}.json`));
@@ -20,10 +22,19 @@ const Mock = require('mockjs'),
 		    	let dataFormatted = JSON.parse(fs.readFileSync(`./data/${value}.json`));
 		        var text = Mock.mock(dataFormatted)
 		        res.send(text);
-		    });
-		})
+	    });
+	});
 
-
+	_.map(actionRoutes, (value, name)=>{
+		app.get(name , function(req, res) {
+			const action = require('./actions/' + value)
+	        try{
+	        	res.send(sendSuccess(action()));
+	        }catch(e){
+	        	res.send(sendError('test'));
+	        }
+        });
+	});
 
 // app.use(router)
 app.listen(3001, function (err, result){
@@ -32,3 +43,17 @@ app.listen(3001, function (err, result){
 })
 
 module.exports = app;
+
+function sendSuccess(data={}) {
+	return {
+		status: true,
+		data: data
+	}
+}
+
+function sendError(reseans=''){
+	return {
+		status: false,
+		reseans: reseans
+	}
+}
