3f99f952ea32357c371308d4a048cc4f50f8e017
john <xiongmeiqiang@xiaomi.com>
2017-03-19 01:31:30 +0800

    用mongo来改造数据存储

--- db/insert.js
+++ db/insert.js
@@ -1,9 +1,13 @@
 const connectToDb = require('./index'),
+  _ = require('lodash'),
   assert = require('assert')
 
-function insert(name = "documents", insertData, callback = () => {}) {
+const getRoutes = require('../route/get'),
+      postRoutes = require('../route/post')
 
-  connectToDb(function(db, callback) {
+function insert(insertData, name = "documents", callback = (data) => {}) {
+
+  connectToDb(function(db) {
     // Get the documents collection 
     var collection = db.collection(name);
     // Insert some documents 
@@ -18,6 +22,26 @@ function insert(name = "documents", insertData, callback = () => {}) {
   });
 }
 
-insert('documents', {a:2343}, function(result){
-  console.log(result,'xiong')
-})
+
+function makeDocument(path = '', type = '', querys = [], results = {}) {
+  return {
+    path: path,
+    type: type,
+    querys: querys,
+    results: results
+  }
+}
+
+module.exports = insert;
+
+// _.map(getRoutes, (value, name)=>{
+//   let doc = makeDocument(name, 'get', [], require(`../data/${value}.json` ));
+//   insert(doc, 'documents' , function(result){
+//   });
+// });
+
+// _.map(postRoutes, (value, name)=>{
+//   let doc = makeDocument(name, 'post', [], require(`../data/${value}.json` ));
+//   insert(doc, 'documents', function(result){
+//   });
+// });
--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const Mock = require('mockjs'),
-	_ = require('underscore'),
+	_ = require('lodash'),
 	delay = require('express-delay'),
 	posts = require('./route/post'),
 	http = require('http'),
 	gets = require('./route/get'),
 	express = require('express'),
 	fs = require('fs'),
-	actionRoutes = require('./route/actionroutes')
+	actionRoutes = require('./route/actionroutes'),
+	query = require('./db/query'),
 	app = express();
 	app.use(delay(200, 1000))
 
-	_.each(posts, function (value, name) {
-	    app.post(name , function(req, res) {
-	    	let dataFormatted = JSON.parse(fs.readFileSync(`./data/${value}.json`));
-	        var text = Mock.mock(dataFormatted)
-	        res.send(text);
-	    });
-	})
-	_.each(gets, function (value, name) {
-		    app.get(name , function(req, res) {
-		    	let dataFormatted = JSON.parse(fs.readFileSync(`./data/${value}.json`));
-		        var text = Mock.mock(dataFormatted)
-		        res.send(text);
-	    });
+	query({}, function(data=[]){
+		data.map((item)=>{
+			if(item.type === 'get'){
+			    app.get(item.path , function(req, res) {
+			    	query({path: item.path, type: 'get'} , function(data={}){
+			    		data = data[0] || {};
+			        	var text = Mock.mock(data.results)
+			        	res.send(text);
+		        	});
+			    });
+			}
+			if(item.type === 'post'){
+			    app.post(item.path , function(req, res) {
+			    	query({path: item.path, type: 'get'} , function(data={}){
+			    		data = data[0] || {};
+			        	var text = Mock.mock(data.results)
+			        	res.send(text);
+		        	});
+			    });
+			}
+		})
+
 	});
 
 	_.map(actionRoutes, (value, name)=>{
